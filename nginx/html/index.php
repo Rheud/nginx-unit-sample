7fe31940ea41b11bb0c356e084d7f8c96dc3491d
Rheud <stee1.0f.destiny@gmail.com>
2024-01-02 13:20:54 +0900

    httpとhttpsの差がわかりやすいようにHTMLを変更

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -1,19 +1,24 @@
 <?php
 
 $msg = 'Hello, PHP on Unit!';
-$img_url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . '/newyear2024.jpg';
+$url_base = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
+$css_url = $url_base . './style.css';
+$img_url = $url_base . '/newyear2024.jpg';
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>PHPテスト</title>
+        <link rel="stylesheet" href="<?php echo $css_url; ?>">
     </head>
     <body>
-        <h1>PHPテスト</h1>
-        <p><?php echo $msg; ?></p>
-        <div>
-           <img src="<?php echo $img_url; ?>" alt="初日の出" />
-        </div>
+        <main>
+            <h1>PHPテスト</h1>
+            <p><?php echo $msg; ?></p>
+            <div>
+                <img src="<?php echo $img_url; ?>" alt="初日の出" />
+            </div>
+        </main>
     </body>
 </html>
